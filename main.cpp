1a39647aca05e6d49c45045dfa48a8cfa90b640f
utommo <thomasthuesen@hotmail.com>
2014-12-04 19:02:29 +0100

    Changed it to OO

--- main.cpp
+++ main.cpp
@@ -5,47 +5,68 @@ using namespace std;
 
 #define WIDTH 800
 #define HEIGHT 600
-#define TRUE 1
-#define FALSE 0
+
+class GObject{
+public:
+    int x,y;
+    void update();
+
+};
+class Ball : public GObject{
+public:
+    Ball(){
+        this->x = 50;
+        this->y = 50;
+
+        this->r = 25;
+        this->speed_x = (rand() % 6);
+        this->speed_y = (rand() % 6);
+    }
+    void update(){
+        if(this->x > (WIDTH - this->r) || this->x < this->r){
+            this->speed_x = this->speed_x * -1;
+        }
+        if((this->y > (HEIGHT - 75)) || ((this->y < this->r))){
+            this->speed_y = this->speed_y * -1;
+        }
+        this->x += this->speed_x;
+        this->y += this->speed_y;
+        circle(this->x, this->y, this->r);
+    }
+private:
+    int r, speed_x, speed_y;
+};
+
+class Paddle : public GObject{
+public:
+    Paddle(){
+        this->y = HEIGHT - 50;
+        this->x = 0;
+    }
+    void update();
+};
+
+Ball mainBall;
+Paddle mainPaddle;
+
+void Paddle :: update(){
+    this->x = mainBall.x;
+    line(this->x + 50, this->y, this->x-50, this->y);
+}
 
 int main(){
-
-    int circle_x, circle_y, circle_r, speed_x, speed_y;
-
-    circle_x = 50;
-    circle_y = 100;
-    circle_r = 25;
-    speed_x = (rand() % 3);
-    speed_y = (rand() % 2);
-
-    //For memory purpose:
-    //ballstate = 0 ++
-    //ballstate = 1 +-
-    //ballstate = 2 -+
-    //ballstate = 3 --
 
 
     initwindow(WIDTH,HEIGHT);
     setcolor(YELLOW);
 
-    while(TRUE){
+    while(true){
         Sleep(5);
         cleardevice();
 
-        circle_x = circle_x + speed_x;
-        circle_y = circle_y + speed_y;
+        mainBall.update();
+        mainPaddle.update();
 
-        if(circle_x > (WIDTH - circle_r) || circle_x < circle_r){
-            speed_x = speed_x * -1;
-        }
-        if((circle_y > (HEIGHT - 75)) || ((circle_y < circle_r))){
-            speed_y = speed_y * -1;
-        }
-
-        //Draws the ball
-        circle(circle_x, circle_y, circle_r);
-        //Draws the paddle
-        line((circle_x + 50), (HEIGHT - 50), (circle_x - 50), (HEIGHT - 50));
     }
 
     getch();
